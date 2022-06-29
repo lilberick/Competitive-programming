d4697743d8fd583b1a48dedbd391d830adc653c8
lilberick <lilberick@gmail.com>
2022-06-29 03:10:57 -0500

    1720: Decode XORed Array

--- online-judge-solutions/LeetCode/1720.cpp
+++ online-judge-solutions/LeetCode/1720.cpp
@@ -1,19 +1,13 @@
 //https://leetcode.com/problems/decode-xored-array
 //Lang		:	C++
-//Time		:	1633 ms
-//Memory	:	26 MB
+//Time		:	42 ms
+//Memory	:	25.9 MB
 #include<vector>
 class Solution{
 	public:
 	    vector<int> decode(vector<int>& v, int first){
 		    vector<int> w(1,first);
-		    int s=first;
-		    for(int x:v){
-			int t=0;
-			while((s^t)!=x){t++;}
-			s=t;
-			w.push_back(t);
-		    }
+		    for(int x:v) w.push_back(w.back()^x);
 		    return w;
 	    }
 };
