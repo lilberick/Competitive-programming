a408858807909b13657805a5765b1d176faf9acd
lilberick <lilberick@gmail.com>
2022-10-05 10:18:25 -0500

    1735A: Working Week

---
+++ online-judge-solutions/Codeforces/1735A.cpp
@@ -0,0 +1,14 @@
+//https://codeforces.com/problemset/problem/1735/A
+//Lang		:	GNU C++14
+//Time		:	15 ms
+//Memory	:	0 KB
+#include<iostream>
+using namespace std;
+int main(){
+	int t,n;
+	for(cin>>t;t--;){
+		cin>>n;
+		cout<<n/3-2<<endl;
+	}
+	return 0;
+}
