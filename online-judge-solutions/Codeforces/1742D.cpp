2b9d919cb5555ea19cedcdaa25097f22820d13d0
lilberick <lilberick@gmail.com>
2022-10-13 13:09:21 -0500

    1742D: Coprime

---
+++ online-judge-solutions/Codeforces/1742D.cpp
@@ -0,0 +1,25 @@
+//https://codeforces.com/problemset/problem/1742/D
+//Lang		:	GNU C++14
+//Time		:	514 ms
+//Memory	:	1000 KB
+#include<iostream>
+#include<algorithm>
+using namespace std;
+#define fastInp cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+int main(){
+	fastInp;
+	int t,n,x;
+	for(cin>>t;t--;){
+		cin>>n;
+		int r=-1;
+		int a[1001]={0};
+		for(int i=1;i<=n;i++){cin>>x;a[x]=i;}
+		for(int i=1;i<1000;i++){
+			for(int j=1;j<1000;j++){
+				if(a[i] && a[j] && __gcd(i,j)==1) r=max(r,a[i]+a[j]);
+			}
+		}
+		cout<<r<<endl;
+	}
+	return 0;
+}
