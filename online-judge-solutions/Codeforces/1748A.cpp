7fa197a5341be127f5a677fb398ad17bf858535f
lilberick <lilberick@gmail.com>
2022-11-14 11:27:55 -0500

    1748A: The Ultimate Square

---
+++ online-judge-solutions/Codeforces/1748A.cpp
@@ -0,0 +1,14 @@
+//https://codeforces.com/problemset/problem/1748/A
+//Lang		:	GNU C++14
+//Time		:	46 ms
+//Memory	:	0 KB
+#include<iostream>
+using namespace std;
+int main(){
+	int t,n;
+	for(cin>>t;t--;){
+		cin>>n;
+		cout<<(n+1)/2<<endl;
+	}
+	return 0;
+}
