78f73378c872a1dd13239dbd3c9315a462d0bad3
lilberick <lilberick@gmail.com>
2022-12-10 14:07:17 -0500

    2235: Add Two Integers

---
+++ online-judge-solutions/LeetCode/2235.cpp
@@ -0,0 +1,10 @@
+//https://leetcode.com/problems/add-two-integers/description/
+//Lang		:	C++
+//Time		:	0 ms
+//Memory	:	6 MB
+class Solution {
+	public:
+		int sum(int num1, int num2) {
+			return num1+num2;
+		}
+};
