e52cb3db787d53bbdd0892cfaa73d75f245fd8be
lilberick <lilberick@gmail.com>
2022-12-29 16:28:18 -0500

    535: Encode and Decode TinyURL

---
+++ online-judge-solutions/LeetCode/535.cpp
@@ -0,0 +1,9 @@
+//https://leetcode.com/problems/encode-and-decode-tinyurl/description/
+//Lang		:	C++
+//Time		:	4 ms
+//Memory	:	6.5 MB
+class Solution{
+	public:
+		string encode(string s){return s;}
+		string decode(string s){return s;}
+};
