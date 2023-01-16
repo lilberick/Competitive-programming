cd230aaaf76a1d3922b615ece1817a484736c2e3
lilberick <lilberick@gmail.com>
2023-01-15 21:06:18 -0500

    513A: Game

---
+++ online-judge-solutions/Codeforces/513A.cpp
@@ -0,0 +1,11 @@
+//https://codeforces.com/problemset/problem/513/A
+//Lang		:	GNU C++14
+//Time		:	15 ms
+//Memory	:	0 KB
+#include<iostream>
+using namespace std;
+int main(){
+	int n1,n2,k1,k2;cin>>n1>>n2>>k1>>k2;
+	cout<<(n1>n2?"First":"Second")<<endl;
+	return 0;
+}
