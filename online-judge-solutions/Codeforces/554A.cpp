cf079678072870d49dff089e1ec2474f06b62a9b
lilberick <lilberick@gmail.com>
2023-01-16 13:38:54 -0500

    554A: Kyoya and Photobooks

---
+++ online-judge-solutions/Codeforces/554A.cpp
@@ -0,0 +1,11 @@
+//https://codeforces.com/problemset/problem/554/A
+//Lang		:	GNU C++14
+//Time		:	15 ms
+//Memory	:	0 KB
+#include<iostream>
+using namespace std;
+int main(){
+	string s;cin>>s;
+	cout<<26+s.size()*25<<endl;
+	return 0;
+}
