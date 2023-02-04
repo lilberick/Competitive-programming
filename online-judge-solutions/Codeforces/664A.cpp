bb21fd7eee1d38d2e7fc788bc1b94bda0a1a7252
lilberick <lilberick@gmail.com>
2023-02-04 13:31:58 -0500

    664A: Complicated GCD

---
+++ online-judge-solutions/Codeforces/664A.cpp
@@ -0,0 +1,11 @@
+//https://codeforces.com/problemset/problem/664/A
+//Lang		:	GNU C++14
+//Time		:	15 ms
+//Memory	:	1000 KB
+#include<iostream>
+using namespace std;
+int main(){
+	string s1,s2;cin>>s1>>s2;
+	cout<<((s1==s2)?s1:"1")<<endl;
+	return 0;
+}
