3d838ff35db974eba86ff05930f6bf3107eb0724
lilberick <lilberick@gmail.com>
2023-02-11 11:21:01 -0500

    678A: Johny Likes Numbers

---
+++ online-judge-solutions/Codeforces/678A.cpp
@@ -0,0 +1,11 @@
+//https://codeforces.com/problemset/problem/678/A
+//Lang		:	GNU C++14
+//Time		:	15 ms
+//Memory	:	0 KB
+#include<iostream>
+using namespace std;
+int main(){
+	int n,k;cin>>n>>k;
+	cout<<(n/k+1)*k<<endl;
+	return 0;
+}
