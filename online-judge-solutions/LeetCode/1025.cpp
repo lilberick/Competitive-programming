b1a59f4104a289046e963d21a44cd6a2f7492068
lilberick <lilberick@gmail.com>
2023-03-04 20:38:48 -0500

    1025: Divisor Game

---
+++ online-judge-solutions/LeetCode/1025.cpp
@@ -0,0 +1,10 @@
+//https://leetcode.com/problems/divisor-game
+//Lang		:	C++
+//Time		:	0 ms
+//Memory	:	6 MB
+class Solution{
+public:
+    bool divisorGame(int n){
+        return n%2==0;
+    }
+};
